2365f5ed9591397e2d639567813588e055824b9f
Aqmal <50930584+kifasu@users.noreply.github.com>
2023-08-05 19:55:29 +0800

    (feat) updated version - ready for release

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,13 @@
 import AppRoot from "./containers/AppRoot";
 
+import packageJson from "../package.json";
+import { useEffect } from "react";
+
 const App = () => {
+  useEffect(() => {
+    console.log(`${packageJson.version}`);
+  }, []);
+
   return <AppRoot />;
 };
 
