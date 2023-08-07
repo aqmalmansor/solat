c7bd31ba4e73d87923d2691a293648d0841ae962
Aqmal <50930584+kifasu@users.noreply.github.com>
2023-08-07 14:58:34 +0800

    (feat) added an offline/online mode

--- src/navigation/Routes.tsx
+++ src/navigation/Routes.tsx
@@ -1,15 +1,18 @@
-import { FunctionComponent } from 'react';
-import { Routes, Route, Navigate } from 'react-router-dom';
+import { FunctionComponent } from "react";
+import { Routes, Route, Navigate } from "react-router-dom";
 
-import Home from '../containers/Home';
+import HomePage from "../containers/Home";
+import OfflinePage from "containers/Offline";
 
 const NavRoutes: FunctionComponent = () => {
-    return (
-        <Routes>
-            <Route path='/' element={<Home />} />
-            <Route path='*' element={<Navigate replace to='/' />} />
-        </Routes>
-    )
-}
+  return (
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/offline" element={<OfflinePage />} />
+
+      <Route path="*" element={<Navigate replace to="/" />} />
+    </Routes>
+  );
+};
 
 export default NavRoutes;
