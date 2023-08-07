813a940dc8b1e2eeef76865b78436bddd52e6c96
Aqmal <50930584+kifasu@users.noreply.github.com>
2023-08-07 13:30:23 +0800

    (attempt) offline mode #3

--- src/main.tsx
+++ src/main.tsx
@@ -10,26 +10,6 @@ import App from "./App.tsx";
 import "./index.css";
 import "react-toastify/dist/ReactToastify.css";
 
-const displayOfflinePage = () => {
-  const offlineFallbackPage = document.createElement("div");
-  offlineFallbackPage.innerHTML = `
-      <!DOCTYPE html>
-      <html lang="en">
-      <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Offline</title>
-      </head>
-      <body>
-        <h1>Offline</h1>
-        <p>You are currently offline. Please check your internet connection.</p>
-      </body>
-      </html>
-    `;
-
-  document.body.appendChild(offlineFallbackPage);
-};
-
 const updateSW = registerSW({
   onNeedRefresh() {
     toast.onChange(() => updateSW(true));
@@ -39,7 +19,7 @@ const updateSW = registerSW({
     });
   },
   onOfflineReady() {
-    displayOfflinePage();
+    alert("Testing offline mode");
   },
 });
 
