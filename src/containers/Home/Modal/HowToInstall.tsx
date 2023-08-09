8faecd2d4db12b28706803b57bed9733ae8646f2
Aqmal <50930584+kifasu@users.noreply.github.com>
2023-08-09 13:06:51 +0800

    (enhanced) ui

--- src/containers/Home/Modal/HowToInstall.tsx
+++ src/containers/Home/Modal/HowToInstall.tsx
@@ -3,10 +3,11 @@ import SVG from "react-inlinesvg";
 
 import Flex from "components/Flex";
 import Modal from "components/Modal";
-import { SPACING } from "entities/tailwind";
+import { ALIGN_ITEMS, JUSTIFY_CONTENT, SPACING } from "entities/tailwind";
 import { useUIStore } from "store/ui";
 import helper, { platforms } from "utils/helper";
 import icons from "assets/icons";
+import Button from "components/Button";
 
 const HowToInstall = (): JSX.Element => {
   const { setInstallationGuideModalOpen } = useUIStore();
@@ -19,15 +20,10 @@ const HowToInstall = (): JSX.Element => {
       return (
         <>
           <Flex noPadding gap={SPACING.extraSmall}>
-            <div>Tap this icon on the address bar</div>
+            <div>1. Tap this icon on the address bar</div>
             <SVG src={icons.A2HS.firefoxMenu} height={20} width={20} />
-            {/* firefoxMenu */}
           </Flex>
-          <Flex noPadding gap={SPACING.extraSmall}>
-            <div>then tap on the 'Install'</div>
-            <SVG src={icons.A2HS.firefoxA2HS} height={20} width={20} />
-            {/* firefoxA2HS */}
-          </Flex>
+          <div>2. Then tap on 'Install'</div>
         </>
       );
     }
@@ -37,14 +33,12 @@ const HowToInstall = (): JSX.Element => {
       return (
         <>
           <Flex noPadding gap={SPACING.extraSmall}>
-            <div>tap on the menu button</div>
+            <div>1. Tap on the menu button</div>
             <SVG src={icons.A2HS.operaMenu} height={20} width={20} />
-            {/* operaMenu */}
           </Flex>
           <Flex noPadding gap={SPACING.extraSmall}>
-            <div>then tap the 'Add to home screen'</div>
+            <div>2. Then tap the 'Add to home screen' button</div>
             <SVG src={icons.A2HS.operaA2HS} height={20} width={20} />
-            {/* operaA2HS */}
           </Flex>
         </>
       );
@@ -55,14 +49,12 @@ const HowToInstall = (): JSX.Element => {
       return (
         <>
           <Flex noPadding gap={SPACING.extraSmall}>
-            <div>Tap this icon on the address bar</div>
+            <div>1. Tap this icon on the address bar</div>
             <SVG src={icons.A2HS.iosShare} height={20} width={20} />
-            {/* iosShare */}
           </Flex>
           <Flex noPadding gap={SPACING.extraSmall}>
-            <div>then tap on the 'Add to Home Screen'</div>
+            <div>2. Then tap on the 'Add to Home Screen'</div>
             <SVG src={icons.A2HS.iosA2HS} height={20} width={20} />
-            {/* iosA2HS */}
           </Flex>
         </>
       );
@@ -70,17 +62,28 @@ const HowToInstall = (): JSX.Element => {
 
     // If browser doesnt support PWA installation
     return (
-      <div>
-        PWA is not supported on this browser. You can try using Google or Edge
-        browser
-      </div>
+      <Flex
+        justify={JUSTIFY_CONTENT.center}
+        align={ALIGN_ITEMS.center}
+        direction="column"
+        salt="min-h-[150px]"
+      >
+        <div>
+          PWA is not supported on this browser. You can try using Google or Edge
+          browser
+        </div>
+        <Button
+          label="Close"
+          onClick={() => setInstallationGuideModalOpen(false)}
+        />
+      </Flex>
     );
   };
 
   return (
     <Modal onClick={() => setInstallationGuideModalOpen(false)}>
-      {platform}
       <Flex direction="column" salt="bg-rose-900" gap={SPACING.small}>
+        <div className="h2 mb-2">How To Install</div>
         {renderInstallationGuide()}
       </Flex>
 
