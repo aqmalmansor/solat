f645ff304d9c1e6f829a07a4c4708fd52bfd0d1f
Aqmal <50930584+kifasu@users.noreply.github.com>
2023-08-09 14:17:43 +0800

    (feat) installation guide modal

--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -1,56 +1,61 @@
-import React from 'react';
-import { Variants, motion } from 'framer-motion';
-import SVG from 'react-inlinesvg';
+import React from "react";
+import { Variants, motion } from "framer-motion";
+import SVG from "react-inlinesvg";
 
-import Button from 'components/Button';
-import Backdrop from './Backdrop';
+import Button from "components/Button";
+import Backdrop from "./Backdrop";
 
-import Motion from 'utils/motion';
+import Motion from "utils/motion";
 
-import Icons from 'assets/icons';
+import Icons from "assets/icons";
 
 interface ModalComponentProps {
-    variants?: Variants;
-    children: React.ReactNode;
-    onClick: () => void;
+  variants?: Variants;
+  children: React.ReactNode;
+  onClick: () => void;
+  height?: React.ComponentProps<"div">["className"];
 }
 
-const Modal = ({ onClick, children, variants }: ModalComponentProps):JSX.Element => {
-    const modalAnimation = variants !== undefined ? variants : undefined;
+const Modal = ({
+  onClick,
+  children,
+  variants,
+  height,
+}: ModalComponentProps): JSX.Element => {
+  const modalAnimation = variants !== undefined ? variants : undefined;
 
-    return (
-        <Backdrop onClick={onClick}>
-            <motion.div
-                onClick={(e) => e.stopPropagation()}
-                className='flex flex-col rounded-[20px] bg-white items-center m-auto min-h-[min(50%,300px)] w-[90%] max-w-[600px]'
-                variants={modalAnimation}
-                initial='hidden'
-                animate='visible'
-                exit='exit'
-            >
-                <div className='absolute top-0 right-0'>
-                    <Button
-                        label={(
-                            <SVG
-                                src={Icons.Cancel}
-                                fill='#7E7E7E'
-                                width={20}
-                                height={20}
-                            />
-                        )}
-                        fill={false}
-                        type='p-3 inline-flex items-center justify-center cursor-pointer'
-                        onClick={() => onClick()}
-                    />
-                </div>
-                {children}
-            </motion.div>
-        </Backdrop>
-    );
+  const modalHeight =
+    height && height.length ? height : "min-h-[min(50%,300px)]";
+
+  return (
+    <Backdrop onClick={onClick}>
+      <motion.div
+        exit="exit"
+        initial="hidden"
+        animate="visible"
+        variants={modalAnimation}
+        onClick={(e) => e.stopPropagation()}
+        className={`m-auto flex w-[90%] max-w-[600px] flex-col items-center rounded-[20px] bg-white ${modalHeight}`}
+      >
+        <div className="absolute right-0 top-0">
+          <Button
+            label={
+              <SVG src={Icons.Cancel} fill="#7E7E7E" width={20} height={20} />
+            }
+            fill={false}
+            type="p-3 inline-flex items-center justify-center cursor-pointer"
+            onClick={() => onClick()}
+          />
+        </div>
+        {children}
+      </motion.div>
+    </Backdrop>
+  );
 };
 
 Modal.defaultProps = {
-    variants: Motion.dropIn,
+  variants: Motion.dropIn,
+  height: undefined,
 };
 
 export default Modal;
--- src/containers/Home/Modal/HowToInstall.tsx
+++ src/containers/Home/Modal/HowToInstall.tsx
@@ -1,6 +1,3 @@
-import { useState } from "react";
-import SVG from "react-inlinesvg";
-
 import Flex from "components/Flex";
 import Modal from "components/Modal";
 import Button from "components/Button";
@@ -13,106 +10,112 @@ import helper, { platforms } from "utils/helper";
 
 import icons from "assets/icons";
 
-const GuideIcon = ({ src }: { src: string }) => {
-  return <SVG src={src} height={20} width={20} fill="#111" />;
-};
-
 const HowToInstall = (): JSX.Element => {
   const { setInstallationGuideModalOpen } = useUIStore();
-
-  const [platform, setPlatform] = useState<string>(helper.getPlatform());
+  const platform = helper.getPlatform();
 
   const renderInstallationGuide = () => {
     // Firefox mobile
     if (platform === platforms.FIREFOX) {
       return (
-        <>
-          <Flex noPadding gap={SPACING.extraSmall}>
-            <div>1. Tap this icon on the address bar</div>
-            <GuideIcon src={icons.A2HS.firefoxMenu} />
-          </Flex>
-          <div>2. Then tap on 'Install'</div>
-        </>
+        <Flex yPadding={SPACING.none}>
+          <ol className="list-decimal space-y-6">
+            <li>
+              Tap this icon on the address bar&nbsp;
+              <img
+                src={icons.A2HS.firefoxMenu}
+                className="inline h-[20px] w-[20px]"
+              />
+            </li>
+            <li>Then tap on 'Install'</li>
+          </ol>
+        </Flex>
       );
     }
 
     // Opera mobile
     if (platform === platforms.OPERA) {
       return (
-        <>
-          <Flex noPadding gap={SPACING.extraSmall}>
-            <div>1. Tap on the menu button</div>
-            <GuideIcon src={icons.A2HS.operaMenu} />
-          </Flex>
-          <Flex noPadding gap={SPACING.extraSmall} salt="flex-wrap">
-            <div>2. Then tap the 'Add to home screen' button</div>
-            <GuideIcon src={icons.A2HS.operaA2HS} />
-          </Flex>
-        </>
+        <Flex yPadding={SPACING.none}>
+          <ol className="list-decimal space-y-6">
+            <li>
+              Tap this icon on the address bar
+              <img
+                src={icons.A2HS.operaMenu}
+                className="inline h-[20px] w-[20px]"
+              />
+            </li>
+            <li>
+              Then tap on the 'Add to Home Screen'&nbsp;
+              <img
+                src={icons.A2HS.operaA2HS}
+                className="inline h-[20px] w-[20px]"
+              />
+            </li>
+          </ol>
+        </Flex>
       );
     }
 
     // IOS devices
     if (platform === platforms.IDEVICE) {
       return (
-        <>
-          <Flex noPadding gap={SPACING.extraSmall}>
-            <div>1. Tap this icon on the address bar</div>
-            <GuideIcon src={icons.A2HS.iosShare} />
-          </Flex>
-          <Flex noPadding gap={SPACING.extraSmall} salt="flex-wrap">
-            <div>2. Then tap on the 'Add to Home Screen'</div>
-            <GuideIcon src={icons.A2HS.iosA2HS} />
-          </Flex>
-        </>
+        <Flex yPadding={SPACING.none}>
+          <ol className="list-decimal space-y-6">
+            <li>
+              Tap this icon on the address bar &nbsp;
+              <img
+                src={icons.A2HS.iosShare}
+                className="inline h-[20px] w-[20px]"
+              />
+            </li>
+            <li>
+              Then tap on the 'Add to Home Screen' &nbsp;
+              <img
+                src={icons.A2HS.iosA2HS}
+                className="inline h-[20px] w-[20px]"
+              />
+            </li>
+          </ol>
+        </Flex>
       );
     }
 
     // If browser doesnt support PWA installation
     return (
       <Flex
-        justify={JUSTIFY_CONTENT.center}
-        align={ALIGN_ITEMS.center}
+        noPadding
         direction="column"
         salt="min-h-[150px]"
+        justify={JUSTIFY_CONTENT.center}
+        align={ALIGN_ITEMS.center}
       >
         <div>
           PWA is not supported on this browser. You can try using Google or Edge
           browser
         </div>
-        <Button
-          label="Close"
-          onClick={() => setInstallationGuideModalOpen(false)}
-        />
       </Flex>
     );
   };
 
   return (
-    <Modal onClick={() => setInstallationGuideModalOpen(false)}>
-      <Flex direction="column" salt="bg-rose-900" gap={SPACING.small}>
-        <div className="h2 mb-2 w-full text-start">How To Install</div>
+    <Modal
+      onClick={() => setInstallationGuideModalOpen(false)}
+      height="min-h-none"
+    >
+      <Flex
+        direction="column"
+        gap={SPACING.extraSmall}
+        justify={JUSTIFY_CONTENT.start}
+        align={ALIGN_ITEMS.start}
+      >
+        <div className="h2 mb-3 w-full font-semibold">How To Install</div>
         {renderInstallationGuide()}
+        <Button
+          label="Close"
+          onClick={() => setInstallationGuideModalOpen(false)}
+        />
       </Flex>
-
-      <button type="button" onClick={() => setPlatform("idevice")}>
-        IDEVICE
-      </button>
-      <button type="button" onClick={() => setPlatform("opera")}>
-        OPERA
-      </button>
-      <button type="button" onClick={() => setPlatform("firefox")}>
-        Firefox
-      </button>
-      <button type="button" onClick={() => setPlatform("firefox_new")}>
-        FF New
-      </button>
-      <button type="button" onClick={() => setPlatform("native")}>
-        Native
-      </button>
-      <button type="button" onClick={() => setPlatform("other")}>
-        Other
-      </button>
     </Modal>
   );
 };
